258b0ada5135c2251258fb0a5806debda24edd9d
kzkick2nd <aruga.kazuki@gmail.com>
2015-12-26 00:23:25 +0900

    Mv header navigation

--- themes/cloneamental/elements/header.php
+++ themes/cloneamental/elements/header.php
@@ -10,20 +10,22 @@ $displayThirdColumn = $blocks > 0 || $c->isEditMode();
     <div class="container">
         <div class="row">
             <div class="col-sm-4 col-xs-6">
-                <?php 
+                <?php
                 $a = new GlobalArea('Header Site Title');
-                $a->display();
-                ?>
-            </div>
-            <div class="<?php  if ($displayThirdColumn) { ?>col-sm-5 col-xs-6<?php  } else { ?>col-sm-8 col-xs-6<?php  } ?>">
-                <?php 
-                $a = new GlobalArea('Header Navigation');
                 $a->display();
                 ?>
             </div>
             <?php  if ($displayThirdColumn) { ?>
                 <div class="col-sm-3 col-xs-12"><?php  $as->display(); ?></div>
             <?php  } ?>
+        </div>
+    </div>
+    <div class="container">
+        <div class="col-sm-12 col-xs-12">
+            <?php
+            $a = new GlobalArea('Header Navigation');
+            $a->display();
+            ?>
         </div>
     </div>
 </header>
